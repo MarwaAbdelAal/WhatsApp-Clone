68cfc1dbbb6f48095d298d4f1184a11a153e07c2
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-06-01 22:18:26 +0300

    authentication

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -15,14 +17,11 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-  const chat = chats.find((c) => c._id === req.params.id);
-  res.send(chat);
-});
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
